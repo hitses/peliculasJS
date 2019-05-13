dbf9d58629b62c83db11d364e177babcf05d0bf6
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-13 17:17:08 +0200

    SCSS de las películas actualizado

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,12 @@ window.addEventListener('load', () => {
   let divFilms = document.querySelector('#peliculas');
   console.log('Funciona');
   films.forEach((film) => {
-    let divFilm = document.createElement('div');
+    let divFilm = document.createElement('article');
     divFilm.innerHTML = `
     <img src="${imgPath}${film.poster_path}" alt="${film.title}">
-    <p>${film.title}</p>
-    <p>${film.vote_average}</p>
-    <p>${film.release_date}</p>
+    <p class="title">${film.title}</p>
+    <p class="ratio">${film.vote_average}</p>
+    <p class="date">${film.release_date}</p>
     `;
 
     divFilms.appendChild(divFilm);
