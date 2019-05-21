4c49210cac24bfcf208d2ea15ccd78c2e4a7c103
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-21 16:22:40 +0200

    Búsqueda actualizada. Eliminado botón de buscar. Botones de ordenar añadidos.

--- scripts/detalle.js
+++ scripts/detalle.js
@@ -1,6 +1,4 @@
-import films from './films.js';
-
-const IMG_PATH = 'http://image.tmdb.org/t/p/w185/'
+const IMG_PATH = 'http://image.tmdb.org/t/p/w500/'
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'a94db6e1acf929e6c3d28e88dc1bb386';
 const API_POPULAR_URL = 'movie/popular';
@@ -8,22 +6,27 @@ const API_GENRE = 'genre/movie/list';
 
 window.addEventListener('load', load);
 
-function load (){                  
-  let url = new URL(window.location.href);
-  let result = url.searchParams.get('id');
-  let pelicula = films.find((iteracion) => result == iteracion.id)
-  let body = document.querySelector('body');
-  let articleFilm = document.createElement('article');
-  let stars = '<i class="fas fa-star"></i>'.repeat((Math.round(pelicula['vote_average'] / 2)));
+function load (){
+  axios.get(API_URL + API_POPULAR_URL + '?api_key=' + API_KEY).then((resp) => {
+    let films = resp.data.results;
+    let url = new URL(window.location.href);
+    let result = url.searchParams.get('id');
+    let pelicula = films.find((iteracion) => result == iteracion.id)
+    let body = document.querySelector('body');
+    let articleFilm = document.createElement('article');
+    let stars = '<i class="fas fa-star"></i>'.repeat((Math.round(pelicula['vote_average'] / 2)));
+    let voidStars = '<i class="far fa-star"></i>'.repeat((5-(Math.round(pelicula['vote_average'] / 2))));
 
-  articleFilm.innerHTML = `
-  <img src="${IMG_PATH}${pelicula.poster_path}" alt="${pelicula.title}">
+    articleFilm.innerHTML = `
     <p class="title">${pelicula.title}</p>
     <div class="stars">
       ${stars}
+      ${voidStars}
     </div>
+    <img class="caratula" src="${IMG_PATH}${pelicula.poster_path}" alt="${pelicula.title}">
     <p class="overview">${pelicula.overview}</p>
     `;
 
     body.appendChild(articleFilm);
+})
 };
--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,54 @@
-import films from './films.js';
-
-const IMG_PATH = 'http://image.tmdb.org/t/p/w185/'
+const IMG_PATH = 'http://image.tmdb.org/t/p/w500/'
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_POPULAR_URL = 'movie/popular';
+const API_KEY = 'a94db6e1acf929e6c3d28e88dc1bb386';
 
 window.addEventListener('load', () => {
-  printFilms(films);
+  axios.get(API_URL + API_POPULAR_URL + '?api_key=' + API_KEY).then((resp) => {
+    let films = resp.data.results;
+    printFilms(films);
 
-  let botonBuscar = document.querySelector('#botonBuscar');
-  botonBuscar.addEventListener('click', buscar);
-});
+    let botonBuscar = document.querySelector('#botonBuscar');
+    botonBuscar.addEventListener('click', buscar);
 
-
-function printFilms(filmsToShow){
-  let sectionFilms = document.querySelector('#peliculas');
-  sectionFilms.innerHTML = '';
-
-  filmsToShow.forEach((film) => {
-    let articleFilm = document.createElement('article');
-    let {poster_path, title, vote_average, release_date, id} = film;
-    let stars = '<i class="fas fa-star"></i>'.repeat((Math.round(film['vote_average'] / 2)));
+    function buscar() {
+      let inputElement = document.querySelector('#inputBusqueda').value;
+      let searchElement = films.filter(film => film.title.toLowerCase().includes(inputElement.toLowerCase()));
+      printFilms(searchElement);
+    }
     
-    articleFilm.innerHTML = `
-    <a href="/pelicula.html?id=${id}" target="_blank"><p class="title">${title}</p></a>
-    <a href="/pelicula.html?id=${id}" target="_blank"><img src="${IMG_PATH}${poster_path}" alt="${title}"></a>
-    <div class="stars">
-      ${stars}
-    </div>
-    <p class="date">${release_date}</p>
-    `;
-
-    sectionFilms.appendChild(articleFilm);
+    let input = document.getElementById("inputBusqueda");
+    input.addEventListener("keyup", function () {
+      document.getElementById("botonBuscar").click();
+    });
   });
-}
 
-function buscar(){
-  let inputElement = document.querySelector('#inputBusqueda').value;
-  let searchElement = films.filter(film => film.title.toLowerCase().includes(inputElement.toLowerCase()));
-  printFilms(searchElement);
-}
+  function printFilms(filmsToShow) {
+    let sectionFilms = document.querySelector('#peliculas');
+    sectionFilms.innerHTML = '';
 
-//Entre para buscar
-let input = document.getElementById("inputBusqueda");
-input.addEventListener("keyup", function() {
-  document.getElementById("botonBuscar").click();
+    filmsToShow.forEach((film) => {
+      let articleFilm = document.createElement('article');
+      let {
+        poster_path,
+        title,
+        release_date,
+        id
+      } = film;
+      let stars = '<i class="fas fa-star"></i>'.repeat((Math.round(film['vote_average'] / 2)));
+      let voidStars = '<i class="far fa-star"></i>'.repeat((5 - (Math.round(film['vote_average'] / 2))));
+
+      articleFilm.innerHTML = `
+      <a href="/pelicula.html?id=${id}" target="_blank"><p class="title">${title}</p></a>
+      <a href="/pelicula.html?id=${id}" target="_blank"><img src="${IMG_PATH}${poster_path}" alt="${title}"></a>
+      <div class="stars">
+        ${stars}
+        ${voidStars}
+      </div>
+      <p class="date">${release_date}</p>
+      `;
+
+      sectionFilms.appendChild(articleFilm);
+    });
+  }
 });
